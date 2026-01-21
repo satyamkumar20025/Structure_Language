70b2e06f26f2293c36677becc0cbbb98885bf2ef
Satyam Kumar <satyamkumar6372@gmail.com>
2026-01-21 16:00:42 +0530

    Update use_groupby_having_orderby.sql

--- use_groupby_having_orderby.sql
+++ use_groupby_having_orderby.sql
@@ -15,3 +15,104 @@ Q13. Find project wise maximum bill amount. Show only projects where maximum bil
 Q14. Find department wise total salary only for departments Java and DevOps. Show only departments where total salary is greater than 1,60,000 and order by department.
 Q15. Find project wise average bill amount where rating is greater than or equal to 4. Show only projects where average bill is greater than 90,000 and order b  y average bill descending.
 
+
+-- create database pds;
+use pds;
+-- 🔹 1. CREATE TABLE – Employee
+
+CREATE TABLE Employee (
+    emp_id INT PRIMARY KEY,
+    name VARCHAR(50),
+    dept VARCHAR(30),
+    city VARCHAR(30),
+    salary INT,
+    experience INT
+);
+
+
+INSERT INTO Employee VALUES
+(101, 'Amit',   'Java',    'Delhi',  75000, 4),
+(102, 'Rohit',  'Java',    'Pune',   90000, 6),
+(103, 'Neha',   'Angular', 'Delhi',  65000, 3),
+(104, 'Pooja',  'Testing', 'Mumbai', 55000, 2),
+(105, 'Karan',  'Java',    'Pune',   80000, 5),
+(106, 'Simran', 'Angular', 'Pune',   72000, 4),
+(107, 'John',   'DevOps',  'Delhi',  95000, 7),
+(108, 'Ali',    'Testing', 'Delhi',  60000, 3),
+(109, 'Priya',  'DevOps',  'Mumbai', 98000, 8),
+(110, 'Ankit',  'Java',    'Mumbai', 70000, 3);
+
+
+-- 🔹 2. CREATE TABLE – Project
+CREATE TABLE Project (
+    project_id INT PRIMARY KEY,
+    project_name VARCHAR(50),
+    hours_worked INT,
+    rating INT,
+    bill_amount INT
+);
+
+
+INSERT INTO Project VALUES
+(1,  'Banking App', 120, 4,  90000),
+(2,  'E-Commerce',  150, 5, 120000),
+(3,  'HR Portal',    90, 3,  60000),
+(4,  'Testing Tool', 70, 4,  40000),
+(5,  'Banking App', 130, 5,  95000),
+(6,  'HR Portal',   110, 4,  75000),
+(7,  'Cloud Mig',   160, 5, 140000),
+(8,  'Testing Tool', 80, 3,  42000),
+(9,  'Cloud Mig',   170, 5, 150000),
+(10, 'E-Commerce',  100, 4,  85000);
+
+select * from employee;
+
+select * from project;
+
+select dept, sum(salary) as total_salary from employee where city in('delhi','pune') 
+group by  dept having sum(salary) > 150000 order by total_salary desc;
+
+select city, sum(salary) as total_salary ,avg(salary) from employee 
+where experience in(3) group by city having avg(salary)<70000 order by total_salary desc;
+ 
+ select project_name , count(hours_worked) as total_hours from project
+ where project_name <>"Testion tool"
+ group by project_name having count(hours_worked) >200 order by total_hours;
+ 
+ 
+ select dept, max(salary) as max_salary from employee 
+ where city in ('delhi','mumbai') group by dept having max(salary)<85000 order by dept;
+ 
+ select city, count(emp_id) as total_employee from employee 
+   where salary between 60000 and 95000 group by city having count(emp_id)>2 order by  total_employee desc;  
+
+select project_name ,avg(rating) as avg_rating from project
+ where hours_worked >90 group by project_name having avg(rating) >=4 order by avg_rating;
+ 
+ 
+select dept, sum(salary) as total_salary from employee 
+where experience >=4 group by dept having sum(salary)>200000 order by total_salary;
+
+select city , min(salary) as min_salary from employee 
+where city not in("delhi") group by city having min(salary) >60000 order by min_salary desc;
+
+select project_name ,sum(bill_amount) as total_amount from project 
+where bill_amount >50000 group by project_name having sum(bill_amount) >100000 order by total_amount;
+
+select dept ,avg(experience) as total_experience from employee
+where city in("pune") group by dept having avg(experience)>=4 order by total_experience;
+
+select city , count(experience) as total_experience from employee
+where experience >=4 group by  city having count(experience) >1 order by total_experience;
+
+select project_name , sum(hours_worked) as total_work from project 
+where project_name not in('hr portal') group by project_name having sum(hours_worked)>150 order by total_work;
+
+select project_name , max(bill_amount) as total_amount from project 
+group by project_name having max(bill_amount) >100000 order by total_amount;
+
+select dept, sum(salary) as total_salary from employee 
+where dept in('java','devops') group by dept having sum(salary)>160000 order by total_salary; 
+
+select project_name , avg(bill_amount) as total_amount from project
+where rating >=4 group by project_name having avg(bill_amount)>90000 order by total_amount;
